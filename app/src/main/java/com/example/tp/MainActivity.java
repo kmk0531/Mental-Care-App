56a1494508a980d210881dfb1beb94dbd9cba430
kimmunki <kmk7072301@gmail.com>
2025-04-03 10:51:29 +0900

    commit test

--- app/src/main/java/com/example/tp/MainActivity.java
+++ app/src/main/java/com/example/tp/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CalendarFragment()).commit();
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int id = item.getItemId();
+
+            if (id == R.id.nav_calendar) {
+                selectedFragment = new CalendarFragment();
+            } else if (id == R.id.nav_chatbot) {
+                selectedFragment = new ChatbotFragment();
+            } else if (id == R.id.nav_map) {
+                selectedFragment = new MapFragment();
+            }
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+
+            return true;
+
+        });
+
     }
 }
